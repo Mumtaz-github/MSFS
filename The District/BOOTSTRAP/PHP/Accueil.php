3b718012c93f58a154174b6d76ed6c13ced968d3
Mumtaz <mumtazfra@gmail.com>
2024-07-30 01:36:35 +0200

    add the new work

--- The District/BOOTSTRAP/PHP/Accueil.php
+++ The District/BOOTSTRAP/PHP/Accueil.php
@@ -1,5 +1,4 @@
-
-<?php $showVideo = true;?>
+<?php $showVideo = true; ?>
 <?php
 require_once('header.php');
 require_once('database.php');
@@ -12,32 +11,20 @@ $bestSellingDishes = $dao->getBestSellingDishes();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Accueil</title>
-
-  <link rel="stylesheet" href="../CSS/plats.css">
-</head>
-
-<body class="parallax">
-  
-  <div class="container py-0">  
+<body id="parallax">
+  <div class="container py-0">
     <h1 class="text-center mb-4">CATÉGORIES POPULAIRES</h1> <!--parite 6 plats-->
     <div class="row row-cols-1 row-cols-md-3 g-5">
       <?php foreach ($categories as $category) : ?>
         <div class="col">
-          <div class="card">
-            <a href="plats.php?category_id=<?= $category['id']; ?>">
+          <a href="plats.php?category_id=<?= $category['id']; ?>">
+            <div class="card">
               <img src="img/<?= $category['image']; ?>" class="card-img-top" alt="<?= $category['libelle']; ?>">
               <div class="card-img-overlay text-center">
                 <h5 class="card-title" style="position: absolute; bottom: 0; left: 0; width: 100%;"><?= $category['libelle']; ?></h5>
               </div>
-            </a>
-          </div>
+            </div>
+          </a>
         </div>
       <?php endforeach; ?>
     </div>
@@ -46,57 +33,44 @@ $bestSellingDishes = $dao->getBestSellingDishes();
     <div class="row row-cols-1 row-cols-md-3 g-5">
       <?php foreach ($bestSellingDishes as $dish) : ?>
         <div class="col">
-        <div class="card plats-card-no-zoom">
-           <a href="plats.php">
+          <a href="plats.php">
+            <div class="card plats-card-no-zoom">
               <img src="img/<?= $dish['image']; ?>" class="dis-img-top" alt="<?= $dish['libelle']; ?>">
               <div class="card-img-overlay text-center">
                 <h5 class="card-title" style="position: absolute; bottom: 0; left: 0; width: 100%;"><?= $dish['libelle']; ?></h5>
               </div>
-            </a>
-          </div>
+            </div>
+          </a>
         </div>
       <?php endforeach; ?>
     </div>
+  </div>
+
+  <!-- Add a search form -->
+  <form action="plats.php" method="get" class="ongletrecherche">
+    <input type="search" name="q" placeholder="Rechercher...">
+    <button type="submit">Rechercher</button>
+  </form>
+
+  <!--Add a container for search results -->
+  <section id="search-results-container">
+    <h2 class="text-center mb-4"></h2>
+    <div id="search-results">
     </div>
-   <!-- Add a search form -->
- <!-- Add a search form -->
- <!-- <form action="plats.php" method="post" class="ongletrecherche">
-  <input type="search" name="q" placeholder="Rechercher...">
-  <button type="submit">Rechercher</button>
-</form> -->
+  </section>
 
-<!--Add a container for search results -->
- <!-- <section id="search-results-container">
-  <h2 class="text-center mb-4"></h2>
-  <div id="search-results"> 
-    <!-- The search results will be displayed here -->
-  <!-- </div>
-</section>  --> -->
-<!-- this is first two were worked or old -->
+  <?php require_once('../PHP/footer.php') ?>
 
+  <!-- <script src="../JAVASCRIPT/searchbar.js"></script> -->
+
+
+
+  <!-- 
  <div class="container mt-3">
   <form action="" method="GET" class="form-inline justify-content-center">
     <input type="text" name="search" class="form-control mr-sm-2" 
-    placeholder="Rechercher..." value="<?php echo htmlspecialchars($searchTerm, ENT_QUOTES, 'UTF-8');?>">
+    placeholder="Rechercher..." value="< ?php echo htmlspecialchars($searchTerm, ENT_QUOTES, 'UTF-8');?>">
       <button type="submit" class="btn btn-primary">Rechercher</button>
   </form>
 </div> 
-    <!-- Add a container for search results -->
-    <!-- <section id="search-results-container">
-      <h2 class="text-center mb-4">Résultats de recherche</h2>
-       <div id="search-results">
-  The search results will be displayed here -->
-      <!-- < /div>
-    </section> --> 
-
-
-
-
-    <?php require_once('../PHP/footer.php') ?>
-    
-    <!-- <script src="../JAVASCRIPT/searchbar.js"></script> -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
+   -->
--- The District/BOOTSTRAP/PHP/plats.php
+++ The District/BOOTSTRAP/PHP/plats.php
@@ -1,17 +1,17 @@
-<?php $showVideo = true;?>
+<?php $showVideo = true; ?>
 <?php
 
 
-  require_once('header.php');
- require_once('database.php');
+require_once('header.php');
+require_once('database.php');
 require_once('DAO.php');
 
 // this functions for to display clickable accueil and categorie pages images, 
 //also display plats.php 6 plats 
- $dao = new DAO($conn);
+$dao = new DAO($conn);
 
 
- if (isset($_GET['q'])) {
+if (isset($_GET['q'])) {  /*for searchbar of page accueil*/
   $searchQuery = $_GET['q'];
   $dishes = $dao->search($searchQuery);
 } elseif (isset($_GET['category_id'])) {
@@ -21,45 +21,20 @@ require_once('DAO.php');
   $dishes = $dao->getAllDishes();
 }
 
-//  if (isset($_GET['category_id'])) {
-//   $category_id = $_GET['category_id'];
+?>
 
-//   // Get dishes for the selected category/
-//   // Get category ID from URL parameter
-//    $dishes = $dao->getDishesByCategory($category_id);
-  
-// } elseif (isset($category_id)) { // added parentheses and isset check
-//    $dishes = $dao->getDishesByCategory($category_id);
-//   // Get 6 dishes from database district
-//   $dishes = $dao->getAllDishes();
-//  } else {
-//   // Get 6 or all dishes from database district
-//  $dishes = $dao->getAllDishes(); //to get 6 dishes getSixDishes
-//  } 
-// ?> 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>PLATS</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="../CSS/plats.css">
-</head>
 
 <body>
   <div class="container py-0">
-  <h1 class="text-center mb-4">TOUTES LES PLATS</h1>
+    <h1 class="text-center mb-4">TOUTES LES PLATS</h1>
     <div id="carouselExample" class="carousel slide text-center" data-bs-ride="carousel">
-      <div class="carousel-inner mb-5 mt-5">  <!--mb mean margin bottom and mt mean margin top-->
+      <div class="carousel-inner mb-5 mt-5"> <!--mb mean margin bottom and mt mean margin top-->
         <?php
         $numSlides = ceil(count($dishes) / 2);
         for ($i = 0; $i < $numSlides; $i++) :
         ?>
           <div class="carousel-item <?= $i === 0 ? 'active' : '' ?>">
-            <div class="row row-cols-1 row-cols-md-2 g-5"> 
+            <div class="row row-cols-1 row-cols-md-2 g-5">
               <?php foreach (array_slice($dishes, $i * 2, 2) as $dish) : ?> <!--if i take this line in comment and below line sans comment , the plats page show all plats in vertical-->
                 <!-- < ?php foreach ($dishes as $dish):?> -->
                 <div class="col">
@@ -78,7 +53,7 @@ require_once('DAO.php');
               <?php endforeach; ?>
             </div>
           </div>
-          <?php endfor; ?>
+        <?php endfor; ?>
       </div>
       <!-- Carousel controls -->
       <div class="container-fluid mt-2 mb-2">
@@ -96,13 +71,11 @@ require_once('DAO.php');
         </div>
       </div>
     </div>
-    </div>
-   
+  </div>
+
 
   <!-- ****************************socail media icons***************************-->
   <?php require_once('../PHP/footer.php') ?>
 
-   <script src="../JAVASCRIPT/categplats.JS"></script> 
+  <script src="../JAVASCRIPT/categplats.JS"></script>
   <script src="../JAVASCRIPT/searchbar.js"></script>
-  <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1
